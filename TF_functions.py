185bc40eee205a09a085f76a4540d15526338b9b
jsong55 <54928428+jsong55@users.noreply.github.com>
2022-10-19 15:20:50 -0400

    Update TF_functions.py

--- TF_functions.py
+++ TF_functions.py
@@ -1,141 +1,46 @@
-import zipfile
-import os
-import random
+### We create a bunch of helpful functions throughout the course.
+### Storing them here so they're easily accessible.
+
 import tensorflow as tf
-import pathlib
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import datetime
-import tensorflow_hub as hub
-import itertools
-from sklearn.metrics import confusion_matrix
 
-def view_random_image(target_dir,target_class):
-  # Setup the target directory
-  target_folder = target_dir+target_class
-
-  #get a random image path
-  random_image = random.sample(os.listdir(target_folder),1)
-
-  #read in the image and plot it using matplotlib
-  img = mpimg.imread(target_folder+"/"+random_image[0])
-  plt.imshow(img)
-  plt.title(target_class)
-  plt.axis("off")
-
-  print(f"image shape: {img.shape}") # show the image shape
-
-  return img
-
-def plot_loss_curves(history):
+# Create a function to import an image and resize it to be able to be used with our model
+def load_and_prep_image(filename, img_shape=224, scale=True):
   """
-  Return separate loss curves for training and validation metrics
-  """
-  loss=history.history["loss"]
-  val_loss=history.history["val_loss"]
-  accuracy=history.history["accuracy"]
-  val_accuracy=history.history["val_accuracy"]
-  epochs=range(len(history.history["loss"]))
+  Reads in an image from filename, turns it into a tensor and reshapes into
+  (224, 224, 3).
 
-  # plot loss
-  plt.figure()
-  plt.plot(epochs,loss,label="training_loss")
-  plt.plot(epochs,val_loss,label="val_loss")
-  plt.title("loss")
-  plt.xlabel("epochs")
-  plt.legend()
-
-  # plot accuracy
-  plt.figure()
-  plt.plot(epochs,accuracy,label="training_accuracy")
-  plt.plot(epochs,val_accuracy,label="val_accuracy")
-  plt.title("accuracy")
-  plt.xlabel("epochs")
-  plt.legend()
-
-  # create a function to import image and resize it to be able to be used in the model
-def load_and_prep_image(filename, img_shape=224,scale=True):
+  Parameters
+  ----------
+  filename (str): string filename of target image
+  img_shape (int): size to resize target image to, default 224
+  scale (bool): whether to scale pixel values to range(0, 1), default True
   """
-  read an image from filename, turns it into a tensor and reshape it
-  to (image_shape, image_shape, color_channels)
-  """
-  # read in the image
+  # Read in the image
   img = tf.io.read_file(filename)
-  # decode the read file into a tensor
-  img = tf.image.decode_image(img,channels=3)
-  # resize the image
-  img = tf.image.resize(img,size=[img_shape,img_shape])
-  # rescale the image (pixel values between 0 and 1)
-if scale:
-  return img/255.
-else:
-  return img
-
-def pred_and_plot(model,filename,class_names):
-  """
-  import and image, make a prediction with model and plots the image
-  with the predicted class as the title
-  """
-  # import the target image and preprocess it
-  img = load_and_prep_image(filename)
-  # make a prediction
-  pred = model.predict(tf.expand_dims(img,axis=0))
-
-  # add in logic for multi class
-  if len(pred[0])>1:
-    pred_class = class_names[tf.argmax(pred[0])]
+  # Decode it into a tensor
+  img = tf.image.decode_jpeg(img)
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape])
+  if scale:
+    # Rescale the image (get all values between 0 and 1)
+    return img/255.
   else:
-  # get the predicted class
-    pred_class = class_names[int(tf.round(pred))]
+    return img
 
-
-  # plot the image
-  plt.imshow(img)
-  plt.title(f"Prediction: {pred_class}")
-  plt.axis(False)
-  return 0
-
-# Create TensorBoard callback (for multiple models)
-def create_tensorboard_callback(dir_name,experiment_name):
-  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-  tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir = log_dir)
-  print(f"Saving TensorBoard log files to: {log_dir}")
-  return tensorboard_callback
-
-# Let's make a function to create a model from a url
-def create_model(model_url,num_classes=10):
-  """
-  Takes a TensoeFlow HUB URL and creates a Keras Sequential model with it.
-
-  Args:
-  model_url(str): A Tensorflow hub feature extraction url
-  num_classes(int): number of output neurons in the output layers, should
-    be equal to the number of target classes
-
-  Returns:
-    An uncompiled keras sequential model with model_url as feature extractor layers and 
-      dense output layer with num_class output neurons.
-  """
-  # Download the pretrained model and save it as a keras layer
-  feature_extraction_layer = hub.KerasLayer(model_url,
-                        trainable=False,
-                        name="feature_extraction_layer",
-                        input_shape=IMAGE_SHAPE+(3,)) # freeze the already learned patterns
-
-  # Create our model
-  model = tf.keras.Sequential([
-      feature_extraction_layer,
-      layers.Dense(num_classes,activation="softmax",name="output_layer")
-  ])                  
-  return model
+# Note: The following confusion matrix code is a remix of Scikit-Learn's 
+# plot_confusion_matrix function - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.plot_confusion_matrix.html
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import confusion_matrix
 
 # Our function needs a different name to sklearn's plot_confusion_matrix
 def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
   """Makes a labelled confusion matrix comparing predictions and ground truth labels.
+
   If classes is passed, confusion matrix will be labelled, if not, integer class values
   will be used.
+
   Args:
     y_true: Array of truth labels (must be same shape as y_pred).
     y_pred: Array of predicted labels (must be same shape as y_true).
@@ -147,6 +52,7 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
   
   Returns:
     A labelled confusion matrix plot comparing y_true and y_pred.
+
   Example usage:
     make_confusion_matrix(y_true=test_labels, # ground truth test labels
                           y_pred=y_preds, # predicted labels
@@ -202,8 +108,83 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
   # Save the figure to the current working directory
   if savefig:
     fig.savefig("confusion_matrix.png")
-    
- 
+  
+# Make a function to predict on images and plot them (works with multi-class)
+def pred_and_plot(model, filename, class_names):
+  """
+  Imports an image located at filename, makes a prediction on it with
+  a trained model and plots the image with the predicted class as the title.
+  """
+  # Import the target image and preprocess it
+  img = load_and_prep_image(filename)
+
+  # Make a prediction
+  pred = model.predict(tf.expand_dims(img, axis=0))
+
+  # Get the predicted class
+  if len(pred[0]) > 1: # check for multi-class
+    pred_class = class_names[pred.argmax()] # if more than one output, take the max
+  else:
+    pred_class = class_names[int(tf.round(pred)[0][0])] # if only one output, round
+
+  # Plot the image and predicted class
+  plt.imshow(img)
+  plt.title(f"Prediction: {pred_class}")
+  plt.axis(False);
+  
+import datetime
+
+def create_tensorboard_callback(dir_name, experiment_name):
+  """
+  Creates a TensorBoard callback instand to store log files.
+
+  Stores log files with the filepath:
+    "dir_name/experiment_name/current_datetime/"
+
+  Args:
+    dir_name: target directory to store TensorBoard log files
+    experiment_name: name of experiment directory (e.g. efficientnet_model_1)
+  """
+  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  tensorboard_callback = tf.keras.callbacks.TensorBoard(
+      log_dir=log_dir
+  )
+  print(f"Saving TensorBoard log files to: {log_dir}")
+  return tensorboard_callback
+
+# Plot the validation and training data separately
+import matplotlib.pyplot as plt
+
+def plot_loss_curves(history):
+  """
+  Returns separate loss curves for training and validation metrics.
+
+  Args:
+    history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
+  """ 
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  accuracy = history.history['accuracy']
+  val_accuracy = history.history['val_accuracy']
+
+  epochs = range(len(history.history['loss']))
+
+  # Plot loss
+  plt.plot(epochs, loss, label='training_loss')
+  plt.plot(epochs, val_loss, label='val_loss')
+  plt.title('Loss')
+  plt.xlabel('Epochs')
+  plt.legend()
+
+  # Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label='training_accuracy')
+  plt.plot(epochs, val_accuracy, label='val_accuracy')
+  plt.title('Accuracy')
+  plt.xlabel('Epochs')
+  plt.legend();
+
 def compare_historys(original_history, new_history, initial_epochs=5):
     """
     Compares two TensorFlow model History objects.
@@ -255,16 +236,22 @@ import zipfile
 def unzip_data(filename):
   """
   Unzips filename into the current working directory.
+
   Args:
     filename (str): a filepath to a target zip folder to be unzipped.
   """
   zip_ref = zipfile.ZipFile(filename, "r")
   zip_ref.extractall()
   zip_ref.close()
-  
+
+# Walk through an image classification directory and find out how many files (images)
+# are in each subdirectory.
+import os
+
 def walk_through_dir(dir_path):
   """
   Walks through dir_path returning its contents.
+
   Args:
     dir_path (str): target directory
   
@@ -283,9 +270,11 @@ from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 def calculate_results(y_true, y_pred):
   """
   Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+
   Args:
       y_true: true labels in the form of a 1D array
       y_pred: predicted labels in the form of a 1D array
+
   Returns a dictionary of accuracy, precision, recall, f1-score.
   """
   # Calculate model accuracy
