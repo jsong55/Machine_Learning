fd3a2f31dd032137f0e01594ea5ef66d0907cb03
jsong55 <54928428+jsong55@users.noreply.github.com>
2022-10-23 15:48:46 -0400

    Update TF_functions.py

--- TF_functions.py
+++ TF_functions.py
@@ -286,3 +286,20 @@ def calculate_results(y_true, y_pred):
                   "recall": model_recall,
                   "f1": model_f1}
   return model_results
+
+def view_random_image(target_dir,target_class):
+  # Setup the target directory
+  target_folder = target_dir+target_class
+
+  #get a random image path
+  random_image = random.sample(os.listdir(target_folder),1)
+
+  #read in the image and plot it using matplotlib
+  img = mpimg.imread(target_folder+"/"+random_image[0])
+  plt.imshow(img)
+  plt.title(target_class)
+  plt.axis("off")
+
+  print(f"image shape: {img.shape}") # show the image shape
+
+  return img
